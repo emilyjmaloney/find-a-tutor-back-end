6cf8f9a6005420f3d19013b5152461192e8fde4b
sarahmachicado <colorsthemind@gmail.com>
2020-09-08 19:27:16 +0000

    added serialze sent messages and received messages to user table

--- src/main.py
+++ src/main.py
@@ -11,7 +11,6 @@ from admin import setup_admin
 from models import db, User, Message, UserProfile, Student, Tutor
 from flask_jwt_simple import JWTManager, create_jwt, get_jwt_identity, jwt_required #added to match the newly added endponts for new user and login
 #from models import Person
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -19,31 +18,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # jwt_simple config
 app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
 jwt = JWTManager(app)
-
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
-
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
-
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
-
 @app.route('/signup', methods=['POST'])     #not from website, Ernesto created, doesn't use jwt to create users
 def handle_signup():
     input_data = request.json
@@ -76,7 +68,6 @@ def handle_signup():
               daily_timeslot = None,
               online = None,
               zipcode = None
-
             )
             db.session.add(new_userprofile)
             db.session.commit()
@@ -104,21 +95,17 @@ def handle_signup():
         return jsonify({
             "msg": "check your keys..."
         }), 400
-
 @app.route('/login', methods=['POST'])
 def login():
     if not request.is_json:
         return jsonify({"msg": "Missing JSON in request"}), 400
-
     params = request.json
     email = params.get('email', None)
     password = params.get('password', None)
-
     if not email:
         return jsonify({"msg": "Missing username parameter"}), 400
     if not password:
         return jsonify({"msg": "Missing password parameter"}), 400
-
     specific_user = User.query.filter_by(
         email_address=email
     ).one_or_none()
@@ -126,10 +113,8 @@ def login():
         if specific_user.password == password:
             # oh, this person is who it claims to be!
             # Identity can be any data that is json serializable
-
             response = {'jwt': create_jwt(identity=specific_user.id), "user": specific_user.serialize()}
             return jsonify(response), 200
-
         else:
             return jsonify({
             "msg": "bad credentials"
@@ -138,10 +123,9 @@ def login():
         return jsonify({
             "msg": "bad credentials"
         }), 400
-
     # if username != 'test' or password != 'test':
     #     return jsonify({"msg": "Bad username or password"}), 401
-@app.route('/protected', methods=['GET'])
+@app.route('/get-single-user', methods=['GET'])
 @jwt_required
 def protected():
     # Access the identity of the current user with get_jwt_identity
@@ -155,11 +139,9 @@ def protected():
             "msg": "user not found"
         }), 404
     else:
-        return jsonify({
-            "msg": "Yay! You sent your token correctly so I know who you are!",
-            "user_data": specific_user.serialize()
-        }), 200
-
+        return jsonify(
+            specific_user.serialize()
+        ), 200
 @app.route('/getall', methods=['GET'])
 def handle_getall():
     users = User.query.all()
@@ -167,14 +149,11 @@ def handle_getall():
     for user in users:
         serialized_users.append(user.serialize())
     return jsonify(serialized_users), 200
-
 @app.route('/messages', methods=['POST'])
 @jwt_required
 def messages():
-
     # First we get the payload json
     body = request.get_json()
-
     if body is None:
         raise APIException("You need to specify the request body as a json object", status_code=400)
     if 'text' not in body:
@@ -183,24 +162,20 @@ def messages():
         raise APIException('You need to specify the created at', status_code=400)
     if 'recipient_id' not in body:
         raise APIException('You need to specify the recipient id', status_code=400)
-
     # at this point, all data has been validated, we can proceed to inster into the bd
     user1 = Message(text=body['text'], created_at=body['created_at'], recipient_id=body['recipient_id'], sender_id=get_jwt_identity())
     db.session.add(user1)
     db.session.commit()
     return "ok", 200
-
 @app.route('/user-profile', methods=['GET'])
 def handle_profile():
     """
     Create person and retrieve all persons
     """
-
     # GET request
     all_people = UserProfile.query.all()
     all_people = list(map(lambda x: x.serialize(), all_people))
     return jsonify(all_people), 200
-
 @app.route('/update-student/<int:id>', methods=["PATCH"])
 def update_student(id):
     body = request.get_json()
@@ -212,7 +187,6 @@ def update_student(id):
         student.grade = body["grade"]
     db.session.commit()
     return jsonify(student.serialize()), 200
-
 @app.route('/update-tutor/<int:id>', methods=["PATCH"])
 def update_tutor(id):
     body = request.get_json()
@@ -223,7 +197,6 @@ def update_tutor(id):
         tutor.experience = body["experience"]
     db.session.commit()
     return jsonify(tutor.serialize()), 200
-
 @app.route('/update-userprofile/<int:id>', methods=["PATCH"])
 def update_user_profile(id):
     body = request.get_json()
@@ -246,7 +219,6 @@ def update_user_profile(id):
         user_profile.zipcode = body["zipcode"]
     db.session.commit()
     return jsonify(user_profile.serialize()), 200
-
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
